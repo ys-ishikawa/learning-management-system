d701ee8365a4a5957af2a9e99b4926dbaff3645d
ys-ishikawa <yosuke1028.ishikawa@gmail.com>
2020-06-19 16:19:56 +0900

    テンプレートの修正およびbootstrapデザインの作成

--- learning-management-system/src/App.js
+++ learning-management-system/src/App.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
+const App = () => {
+
+  const [studyType, setStudyType] = useState('')
+  const [teachingMaterial, setTeachingMaterial] = useState('')
+  const [materialType, setMaterialType] = useState('')
+
   return (
-    <p>This is a templates.</p>
-  );
+    <>
+    <div className="container-fluid">
+      <h3>学習管理システム</h3>
+      <form>
+        <div className="form-group">
+          <label htmlFor="studyType">学習種別</label>
+          <input className="form-control" id="studyType" value={studyType}/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="teachingMaterial">学習教材</label>
+          <input className="form-control" id="teachingMaterial" value={teachingMaterial} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="materialType">教材種別</label>
+          <input className="form-control" id="materialType" value={materialType} />
+        </div>
+      </form>
+      <button className="btn btn-primary">学習イベントを作成する</button>
+      <button className="btn btn-danger">全てのイベントを削除する</button>
+      <hr/>
+      <h3>学習項目一覧</h3>
+      <table class="table table-hover">
+        <thead>
+          <tr><th>ID</th><th>学習種別</th><th>学習教材</th><th>教材種別</th><th></th></tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+
+
+    </div>
+    </>
+  )
 }
 
-export default App;
+export default App
