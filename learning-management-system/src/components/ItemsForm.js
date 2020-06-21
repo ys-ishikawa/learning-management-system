25076b86d802a3703771aaf8bf39faec22d76e09
ys-ishikawa <yosuke1028.ishikawa@gmail.com>
2020-06-21 09:33:01 +0900

    教材種別をドロップダウンに変更

--- learning-management-system/src/components/ItemsForm.js
+++ learning-management-system/src/components/ItemsForm.js
@@ -33,19 +33,23 @@ const ItemsForm = () => {
         <>
             <h3>学習管理システム</h3>
             <form>
-            <div className="form-group">
-                <label htmlFor="studyType">学習種別</label>
-                <input className="form-control" id="studyType" value={studyType} onChange={e => setStudyType(e.target.value)}/>
-            </div>
-            <div className="form-group">
-                <label htmlFor="teachingMaterial">学習教材</label>
-                <input className="form-control" id="teachingMaterial" value={teachingMaterial} onChange={e => setTeachingMaterial(e.target.value)} />
-            </div>
-            <div className="form-group">
-                <label htmlFor="materialType">教材種別</label>
-                {/* TODO: ドロップダウン形式に修正 */}
-                <input className="form-control" id="materialType" value={materialType} onChange={e => setMaterialType(e.target.value)} />
-            </div>
+              <div className="form-group">
+                  <label htmlFor="studyType">種別（言語）</label>
+                  <input className="form-control" id="studyType" value={studyType} onChange={e => setStudyType(e.target.value)}/>
+              </div>
+              <div className="form-group">
+                  <label htmlFor="teachingMaterial">学習教材名</label>
+                  <input className="form-control" id="teachingMaterial" value={teachingMaterial} onChange={e => setTeachingMaterial(e.target.value)} />
+              </div>
+              <div className="form-group">
+                  <label htmlFor="materialType">教材種別</label>
+                  <select className="custom-select custom-select-lg mb-3" value={materialType} onChange={e => setMaterialType(e.target.value)}>
+                    <option selected>↓↓ 以下から選択してください ↓↓</option>
+                    <option value="書籍">書籍</option>
+                    <option value="Udemy">Udemy</option>
+                    <option value="その他">その他</option>
+                  </select>
+              </div>
             </form>
             <button className="btn btn-primary" onClick={addItem}>学習記録を作成する</button>
             <button className="btn btn-danger" onClick={deleteAllItems}>全ての学習記録を削除する</button>
